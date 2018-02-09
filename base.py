7045232171795a60a1aae5b691448583f7f10272
superspeeder101 <36306837+superspeeder101@users.noreply.github.com>
2018-02-09 11:40:41 -0800

    Update base.py

--- base.py
+++ base.py
@@ -1,9 +1,31 @@
 class Item(object):
-  pass
+  def __init__(self, name, raw_ingredient=False,requirements={}):
+    assert isinstance(requirements,dict)
+    self.name=  name
+    self.raw = raw_ingredient
+    if not self.raw:
+      self.requirements = requirements
   
-class Recipe(object):
-  pass
+  def __repr__(self):
+    return "<Item {}>".format(self.name)
+    
+
+def raw_rescources(itm, earlyout={}):
+  output = earlyout
+  if itm.raw:
+    output = {itm:1}
+    return output
   
-def raw_rescources(recipe):
-  output = {}
+  else:
+    for item in itm.requirements.keys():
+      if item.raw:
+        if item in output.keys():
+          output[item] = output[item] + itm.requirements[item]
+        else:
+          output[item] = itm.requirements[item]
+        
+        
+        
   return output
+
+
